5ce3f3a5b7c4156d8f8018ae8697ac6afdb89121
William Rodrigues <williamrsilva97@gmail.com>
2023-07-07 20:47:36 -0300

    Simulação Primeira Partida

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 from clube import Clube
 from partida import Partida
 
+fla = Clube("Flamengo")
+vas = Clube("Vasco")
+
+r1 = Partida(1, fla, vas, "Maracana")
+
+r1.golC()
+r1.golC()
+r1.golC()
+r1.golC()
+r1.golF()
+
+r1.placar()
+
         
--- partida.py
+++ partida.py
@@ -1,7 +1,7 @@
 from clube import Clube
 
 class Partida():
-    def __init__(self, rodada, casa=Clube(), fora=Clube(), estadio, golCasa=0, golFora=0):
+    def __init__(self, rodada, casa=Clube(" "), fora=Clube(" "), estadio=" ", golCasa=0, golFora=0):
         self.rodada = rodada
         self.casa = casa
         self.fora = fora
@@ -14,14 +14,16 @@ class Partida():
         self.casa.gp += 1
         self.fora.gc += 1
         self.casa.sg += 1
-        self.fora.sg -= 1  
-        
+        self.fora.sg -= 1
+        self.atualizaVED()
+
     def golF(self):
         self.golFora += 1
         self.casa.gc += 1
         self.fora.gp += 1
         self.casa.sg -= 1
         self.fora.sg += 1
+        self.atualizaVED()
     
     def atualizaVED(self):
         if(self.golCasa > self.golFora):
@@ -35,7 +37,9 @@ class Partida():
         if(self.golCasa == self.golFora):
             self.casa.emp += 1
             self.fora.emp += 1
-
+    
+    def placar(self):
+        print(self.casa.nome, " ", self.golCasa, " x " , self.golFora, " ",self.fora.nome)
 
  
     
